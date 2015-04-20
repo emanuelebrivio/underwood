4074f28fb2b533f73ab2dfcee654aa965731bedf
Emanuele Brivio <emanuele@plasticpanda.com>
2015-04-20 17:22:11 +0200

    Fix permissions and remove verbose mode on livecompile
    No more useless lines when compiling. Now on Hyper-V everything should be ok with file permissions

--- bin/livecompile.sh
+++ bin/livecompile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ./node_modules/.bin/static -H '{"Cache-Control": "no-cache, must-revalidate"}' --port 2016 -a 0.0.0.0 &
-./node_modules/.bin/nodemon --exec "sh $(pwd)/bin/build.sh" -e jade,styl,js -V
+./node_modules/.bin/nodemon --exec "sh $(pwd)/bin/build.sh" -e jade,styl,js
