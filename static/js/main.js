ac811f5644d9787048561463697995d3d5d68694
Emanuele Brivio <emanuele@plasticpanda.com>
2015-05-27 17:42:04 +0200

    Add Javaspritz CDN

--- static/js/main.js
+++ static/js/main.js
@@ -5,13 +5,4 @@
 
   'use strict';
 
-  NodeList.prototype.forEach = Array.prototype.forEach;
-  HTMLCollection.prototype.forEach = Array.prototype.forEach;
-
-  NodeList.prototype.on = function (event, listener) {
-    this.forEach(function (el) {
-      el.addEventListener(event, listener);
-    });
-  };
-
 })();
